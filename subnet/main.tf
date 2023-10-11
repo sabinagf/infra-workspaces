9659c5b1a8a229ce5489cda6518c20976d04e8dc
sabinagf <142227408+sabinagf@users.noreply.github.com>
2023-10-11 18:06:25 -0400

    added subnet and vpc

--- subnet/main.tf
+++ subnet/main.tf
@@ -6,4 +6,26 @@ terraform {
       name = "infra-subnet"
     }
   } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+  
+
+resource "aws_subnet" "main" {
+  vpc_id     = "vpc-06aaae5f07a5497ab"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -6,4 +6,19 @@ terraform {
       name = "infra-vpc"
     }
   } 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+} 
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+  
 }
